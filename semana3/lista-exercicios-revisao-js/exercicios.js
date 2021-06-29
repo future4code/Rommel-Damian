9cb55dbb02fbdaf60f6386113f069761e5256897
Jesus Rios <jrios@electrodata.com.pe>
2021-06-29 00:56:18 -0500

    Tarefa Atualizada

--- semana3/lista-exercicios-revisao-js/exercicios.js
+++ semana3/lista-exercicios-revisao-js/exercicios.js
@@ -91,8 +91,7 @@ function checaTriangulo(a, b, c) {
 }
 
 // EXERCÍCIO 09
-function comparaDoisNumeros(num1, num2) {
-  
+function comparaDoisNumeros(num1, num2) {  
   let maiorNumero
   let menorNumero
 
@@ -118,73 +117,174 @@ function comparaDoisNumeros(num1, num2) {
 
 // EXERCÍCIO 10
 function segundoMaiorEMenor(array) {
+  let arraySegundoMaiorMenor = []
+  let done = false;
+  while (!done) {
+    done = true;
+    for (let i = 1; i < array.length; i += 1) {
+      if (array[i - 1] > array[i]) {
+        done = false;
+        let a = array[i - 1];
+        array[i - 1] = array[i];
+        array[i] = a;
+      }
+    }    
+  }
+  arraySegundoMaiorMenor.push(array[array.length-2])
+  arraySegundoMaiorMenor.push(array[1])
 
-  
+  return arraySegundoMaiorMenor;
 }
 
 // EXERCÍCIO 11
 function ordenaArray(array) {
+  let done = false;
+  while (!done) {
+    done = true;
+    for (let i = 1; i < array.length; i += 1) {
+      if (array[i - 1] > array[i]) {
+        done = false;
+        let a = array[i - 1];
+        array[i - 1] = array[i];
+        array[i] = a;
+      }
+    }    
+  }
+  return array
 
 }
 
 // EXERCÍCIO 12
 function filmeFavorito() {
+  
+  return {
+    nome: "O Diabo Veste Prada",
+    ano: 2006,
+    diretor: "David Frankel",
+    atores: ["Meryl Streep", "Anne Hathaway", "Emily Blunt", "Stanley Tucci"]   
+  }
 
 }
 
 // EXERCÍCIO 13
 function imprimeChamada() {
   // "Venha assistir ao filme NOME_DO_FILME, de ANO, dirigido por DIRECAO e estrelado por ELENCO."
+  const filme =  {
+    nome: "O Diabo Veste Prada",
+    ano: 2006,
+    diretor: "David Frankel",
+    atores: ["Meryl Streep", "Anne Hathaway", "Emily Blunt", "Stanley Tucci"]   
+  }
+
+  return `Venha assistir ao filme ${filme.nome}, de ${Number(filme.ano)}, dirigido por ${filme.diretor} e estrelado por ${filme.atores.join(", ")}.`
+
 }
 
 // EXERCÍCIO 14
 function criaRetangulo(lado1, lado2) {
+
+  return{
+    largura:lado1,
+    altura:lado2,
+    perimetro: 2*(lado1+lado2),
+    area:lado1*lado2
+  }
 
 }
 
 // EXERCÍCIO 15
 function anonimizaPessoa(pessoa) {
 
+  return{...pessoa, nome:"ANÔNIMO"}
+
 }
 
 // EXERCÍCIO 16A
 function maioresDe18(arrayDePessoas) {
+  
+  const maiorIdade = arrayDePessoas.filter((pessoa)=>{
+    return pessoa.idade >=18
+  })
 
+  return maiorIdade
 }
 
 // EXERCÍCIO 16B
 function menoresDe18(arrayDePessoas) {
+  const menorIdade = arrayDePessoas.filter((pessoa)=>{
+    return pessoa.idade < 18
+  })
 
+  return menorIdade
 }
 
 // EXERCÍCIO 17A
 function multiplicaArrayPor2(array) {
-
+  const multiplicaPor2 = array.map((numero)=>{
+    return numero * 2
+  })
+  return multiplicaPor2
 }
 
 // EXERCÍCIO 17B
 function multiplicaArrayPor2S(array) {
-
+  const multiplicaPor2 = array.map((numero)=>{
+    let numeroString = numero * 2
+    return numeroString.toString()
+  })
+  return multiplicaPor2
 }
 
 // EXERCÍCIO 17C
 function verificaParidade(array) {
-
+  let novoArray = []
+  let resposta
+  for (let i = 0; i < array.length; i++) {    
+    if(array[i]%2===0){
+      resposta = `${array[i]} é par`
+    }else{
+      resposta = `${array[i]} é ímpar`
+    }
+    novoArray.push(resposta)
+  }
+  return novoArray
 }
+
 
 // EXERCÍCIO 18A
 function retornaPessoasAutorizadas(pessoas) {
+
+  const comPermissao = pessoas.filter((pessoa)=>{
+    return pessoa.altura >= 1.5 && pessoa.idade>14 && pessoa.idade<60
+  })
+   return comPermissao
 
 }
 
 // EXERCÍCIO 18B
 function retornaPessoasNaoAutorizadas(pessoas) {
 
+  const SemPermissao = pessoas.filter((pessoa)=>{
+    return pessoa.altura < 1.5 || pessoa.idade<=14 || pessoa.idade>60
+  })
+  return SemPermissao
 }
 
 // EXERCÍCIO 19A
 function ordenaPorNome(consultasNome) {
-
+  let done = false;
+  while (!done) {
+    done = true;
+    for (let i = 1; i < consultasNome.length; i += 1) {
+      if (consultasNome[i - 1].nome > consultasNome[i].nome) {
+        done = false;
+        let a = consultasNome[i - 1].nome;
+        consultasNome[i - 1].nome = consultasNome[i].nome;
+        consultasNome[i].nome = a;
+      }
+    }    
+  }
+  return consultasNome
 }
 
 // EXERCÍCIO 19B

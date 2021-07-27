90ad7b9bf1587ab5a4bbc8ec490b95f234fedb29
Rommel Rios Damian <61754866+Jrios28@users.noreply.github.com>
2021-07-27 16:22:29 -0500

    feito até enunciado, falta desafio

---
+++ semana7/IntegrarAPIComReact/integrandoapireact/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Home from './components/Home'
+
+
+
+class App extends React.Component {
+
+ 
+render() {
+  return (
+    <div className="App">
+  
+      <Home/>
+  
+    </div>
+  );
+  
+ }
+}
+
+export default App;
